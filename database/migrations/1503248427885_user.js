215665b3987d5838f0a1b31b61a9b40326108924
JasonCeng <489281085@qq.com>
2019-04-21 22:00:23 +0800

    npm install mysql&migration:run

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -8,10 +8,12 @@ class UserSchema extends Schema {
     this.create('users', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique().comment('用户登录凭据')
+      table.string('name', 80).notNullable().unique().comment('用户姓名,保留,可追溯') //zengzhaochaung add at 2019/4/21 16:45
       table.integer('sex').defaultTo(0).comment('0:男,1:女')
       table.string('email', 254).notNullable().unique().comment('用户邮箱,保留')
       table.string('phone').defaultTo('').comment('用户手机')
       table.string('password', 60).notNullable()
+      table.integer('role').defaultTo(0).comment('0:老师,1:学生') //zengzhaochaung add at 2019/4/21 16:45
       table.integer('parent_id').defaultTo(0).notNullable().comment('归属导师,默认为0,就是导师自身')
       table.timestamps()
     })
